8698bb74f14631ab56f7772ef786b186e4804d1f
JFCherry <jsquair@gmail.com>
2020-06-28 21:30:37 -0700

    Added two new functions to the join_tweets_and_stocks.py script: load_stock_data() and join_tweets_stock_anomalies_quick().

--- Muskometer/join_tweets_and_stocks.py
+++ Muskometer/join_tweets_and_stocks.py
@@ -31,5 +31,42 @@ def join_tweets_and_stocks(stock_df,tweet_df):
                                             stock_df['DateTime'],row,stock_df))
     return tweet_df
 
+def join_tweets_stocks_anomalies(tweet_df,stock_df,anomalies):
+    # set new column in the tweet data frame to have the stock date
+    tweet_df['stock_time']=tweet_df['Time']\
+                         .apply(lambda row: nearest(stock_df['DateTime'],row))
+    # set new column in the tweet data frame to have the stock price
+    tweet_df['stock_price']=tweet_df['Time']\
+                         .apply(lambda row: nearest_price(\
+                                            stock_df['DateTime'],row,stock_df))
+    # set the tags for which tweets are anomalous
+    tweet_df['anomalous'] = anomalies
+    return tweet_df
+ 
+def load_stock_data(stock_name):
+    stock_df = pd.read_csv('../data/raw/'+stock_name.lower()+'_stock_price.csv')\
+                                .drop('Unnamed: 0',axis='columns')
+    stock_df['DateTime'] = pd.to_datetime(stock_df['DateTime'],utc=True)
+    return stock_df   
+     
+def join_tweets_stocks_anomalies_quick(tweet_df,anomalies,stock_name):
+    stock_df = load_stock_data(stock_name)
+    # send the data we want to arrays
+    tweet_array = tweet_df['Time'].values
+    time_array = stock_df['DateTime'].values
+    stock_time_list = []
+    stock_price_list = []
+    for i in range(len(tweet_array)):
+        bin_now = np.argmin(abs(time_array - tweet_array[i]))
+        stock_time_list.append(stock_df.iloc[bin_now]['DateTime'])
+        stock_price_list.append(stock_df.iloc[bin_now]['Open'])
+    # set new column in the tweet data frame to have the stock date
+    tweet_df['stock_time'] = stock_time_list
+    # set new column in the tweet data frame to have the stock price
+    tweet_df['stock_price'] = stock_price_list
+    # set the tags for which tweets are anomalous
+    tweet_df['anomalous'] = anomalies
+    return tweet_df
+
 if __name__ == '__main__':
     main()
